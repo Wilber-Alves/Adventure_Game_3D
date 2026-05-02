9c90d81680f087505c618a0330636e269220b7d0
Wilber <wilber.sa@hotmail.com>
2026-05-01 21:40:03 -0300

    Update StateMachine.cs

--- Assets/Scripts/EDGEE_CORE/StateMachine.cs
+++ Assets/Scripts/EDGEE_CORE/StateMachine.cs
@@ -4,63 +4,49 @@ using UnityEngine;
 using NaughtyAttributes;
 using System.Runtime.InteropServices.WindowsRuntime;
 
-public class Test
+namespace EDGEE.StateMachine
 {
-    public enum Test2
+
+    public class StateMachine<T> where T : System.Enum
     {
-        NONE,
-        TEST
-    }
+        public Dictionary<T, StateBase> dictionaryStates;
 
-    public void TestMethod()
-    {
-       StateMachine<Test2> stateMachine = new StateMachine<Test2>();
+        private StateBase _currentState;
 
-       stateMachine.RegisterStates(Test.Test2.NONE, new StateBase());
+        public float timeToStartGame = 1.0f;
 
-    }
+        public StateBase CurrentState
+        {
+            get
+            {
+                return _currentState;
+            }
 
-}
-
-public class StateMachine<T> where T : System.Enum
-{ 
-    public Dictionary<T, StateBase> dictionaryStates;
-
-    private StateBase _currentState;
-
-    public float timeToStartGame = 1.0f;
-
-    public StateBase CurrentState
-    {
-        get 
-        { 
-            return _currentState; 
         }
-        
-    }
 
-    public void Init()
-    {
-        dictionaryStates = new Dictionary<T, StateBase>();
-    }
+        public void Init()
+        {
+            dictionaryStates = new Dictionary<T, StateBase>();
+        }
 
-    public void RegisterStates(T typeEnum, StateBase state)
-    {  
-        dictionaryStates.Add(typeEnum, state);
-    }
+        public void RegisterStates(T typeEnum, StateBase state)
+        {
+            dictionaryStates.Add(typeEnum, state);
+        }
 
-    public void SwitchState(T state)
-    {
-        if (_currentState != null) _currentState.OnStateExit();
-        _currentState = dictionaryStates[state];
-        _currentState.OnStateEnter();
-    }
+        public void SwitchState(T state)
+        {
+            if (_currentState != null) _currentState.OnStateExit();
+            _currentState = dictionaryStates[state];
+            _currentState.OnStateEnter();
+        }
 
-    public void Update()
-    {
-      if (_currentState != null)
-      {
-          _currentState.OnStateStay();
-      } 
+        public void Update()
+        {
+            if (_currentState != null)
+            {
+                _currentState.OnStateStay();
+            }
+        }
     }
 }
