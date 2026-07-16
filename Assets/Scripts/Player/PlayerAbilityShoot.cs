c9cf5bc1c18d15450fae48d1c721a6b2d70885c4
Wilber <wilber.sa@hotmail.com>
2026-07-15 22:31:10 -0300

    Update PlayerAbilityShoot.cs
    To better integrate weapon switching using Unity's Input System, I created a new action map within the "inputs" Input Action Asset to handle player weapon selection. By pressing keys 1 through 4, the player can choose between the four created weapons. I modified the *Player Ability Shoots* script to adjust the weapon selection logic and updated all weapon Prefabs.

--- Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -1,43 +1,91 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
-
-    public WeaponBase weaponBase;
+    [Header("Weapons")]
+    public WeaponBase[] weaponBases; // Array with 4 weapons
     public Transform weaponPosition;
 
     private WeaponBase _currentWeapon;
+    private int _currentWeaponIndex = 0;
 
     protected override void Init()
     {
         base.Init();
-        CreateWeapon();
+
+        // Create the first weapon
+        CreateWeapon(_currentWeaponIndex);
+
+        // Shoot input
         inputs.Gameplay.Shoot.performed += ctx => StartShoot();
         inputs.Gameplay.Shoot.canceled += ctx => CancelShoot();
+
+        // Weapon switch inputs (the 4 Actions created)
+        inputs.Player_Weapons.SelectWeapon1.performed += ctx => SwitchWeapon(0);
+        inputs.Player_Weapons.SelectWeapon2.performed += ctx => SwitchWeapon(1);
+        inputs.Player_Weapons.SelectWeapon3.performed += ctx => SwitchWeapon(2);
+        inputs.Player_Weapons.SelectWeapon4.performed += ctx => SwitchWeapon(3);
     }
 
-    private void CreateWeapon()
-    { 
-        _currentWeapon = Instantiate(weaponBase, weaponPosition);
-        _currentWeapon.transform.localPosition = _currentWeapon.transform.localEulerAngles = Vector3.zero;
+    private void OnDestroy()
+    {
+        // Clear events
+        if (inputs != null)
+        {
+            inputs.Gameplay.Shoot.performed -= ctx => StartShoot();
+            inputs.Gameplay.Shoot.canceled -= ctx => CancelShoot();
+
+            inputs.Player_Weapons.SelectWeapon1.performed -= ctx => SwitchWeapon(0);
+            inputs.Player_Weapons.SelectWeapon2.performed -= ctx => SwitchWeapon(1);
+            inputs.Player_Weapons.SelectWeapon3.performed -= ctx => SwitchWeapon(2);
+            inputs.Player_Weapons.SelectWeapon4.performed -= ctx => SwitchWeapon(3);
+        }
     }
 
+    private void SwitchWeapon(int weaponIndex)
+    {
+        if (weaponIndex < 0 || weaponIndex >= weaponBases.Length)
+            return;
+
+        if (weaponIndex == _currentWeaponIndex)
+            return;
+
+        // Destroy the current weapon
+        if (_currentWeapon != null)
+        {
+            Destroy(_currentWeapon.gameObject);
+        }
+
+        // Create the new weapon
+        _currentWeaponIndex = weaponIndex;
+        CreateWeapon(_currentWeaponIndex);
+
+        Debug.Log($"Weapon {weaponIndex + 1} equipped!");
+    }
+
+    private void CreateWeapon(int index)
+    {
+        _currentWeapon = Instantiate(weaponBases[index], weaponPosition);
+        _currentWeapon.transform.localPosition = Vector3.zero;
+        _currentWeapon.transform.localEulerAngles = Vector3.zero;
+    }
 
     private void StartShoot()
     {
-        _currentWeapon.StartShoot();
-        Debug.Log("Shoot");
-    
+        if (_currentWeapon != null)
+        {
+            _currentWeapon.StartShoot();
+            Debug.Log("Shoot");
+        }
     }
 
     private void CancelShoot()
     {
-        _currentWeapon.StopShoot();
-        Debug.Log("Shoot");
+        if (_currentWeapon != null)
+        {
+            _currentWeapon.StopShoot();
+            Debug.Log("Stop Shoot");
+        }
     }
-
-
 }
