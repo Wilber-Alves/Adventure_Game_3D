f97089739282070ad02c6105c15a0bfafc222eaa
Wilber <wilber.sa@hotmail.com>
2026-07-21 23:49:10 -0300

    Create a new Script - BossBase
    The `BossAction` enumeration (Enums) defines the Boss's possible states: `INIT`, `IDLE`, `WALK`, and `ATTACK`. The `BossBase` class is responsible for initializing the state machine using the `StateMachine<BossAction>` class. In the `Init()` method, the state machine is configured, and the `SwitchState(BossAction state)` method allows for changing the current state by passing a reference to the `this` object, which represents the Boss instance.

---
+++ Assets/Scripts/Boss/BossBase.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using EDGEE.StateMachine;
+
+namespace Boss
+{
+
+    public enum BossAction
+    { 
+        INIT,
+        IDLE,
+        WALK,
+        ATTACK
+    }
+
+    public class BossBase : MonoBehaviour
+    {
+        private StateMachine<BossAction> stateMachine;
+
+        private void Init()
+        {
+            stateMachine = new StateMachine<BossAction>();
+            stateMachine.Init();
+
+            stateMachine.RegisterStates(BossAction.INIT, new BossStateBase());
+        }
+
+        #region DEBUG
+        [NaughtyAttributes.Button]
+        private void SwitchInit()
+        { 
+            SwitchState(BossAction.INIT);
+        }
+
+        #endregion
+
+        #region STATE MACHINE
+
+        private void SwitchState(BossAction state)
+        {
+            stateMachine.SwitchState(state, this);
+        }
+
+        #endregion
+    }
+}
