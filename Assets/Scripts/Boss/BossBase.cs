bf77b96b3fceb268990411611394ea40016b2b71
Wilber <wilber.sa@hotmail.com>
2026-07-22 01:32:35 -0300

    Update BossBase.cs

--- Assets/Scripts/Boss/BossBase.cs
+++ Assets/Scripts/Boss/BossBase.cs
@@ -1,5 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using EDGEE.StateMachine;
+using DG.Tweening;
+
 
 namespace Boss
 {
@@ -14,21 +18,72 @@ namespace Boss
 
     public class BossBase : MonoBehaviour
     {
+        [Header("Animation")]
+
+        public float startAnimationDuration = 0.5f;
+        public Ease startAnimationEase = Ease.OutBack;
+
+        public float speed = 5f;
+        public List<Transform> waypoints;
+
+
         private StateMachine<BossAction> stateMachine;
+
+        private void Awake()
+        {
+            Init();
+        }
 
         private void Init()
         {
             stateMachine = new StateMachine<BossAction>();
             stateMachine.Init();
 
-            stateMachine.RegisterStates(BossAction.INIT, new BossStateBase());
+            stateMachine.RegisterStates(BossAction.INIT, new BossStateInit());
+            stateMachine.RegisterStates(BossAction.WALK, new BossStateWalk());
         }
+
+        #region
+
+        public void GoToRandomPoint()
+        {
+            StartCoroutine(GoToPointCoroutine(waypoints[Random.Range(0, waypoints.Count)]));
+        
+        }
+
+        IEnumerator GoToPointCoroutine(Transform t)
+        {
+            while (Vector3.Distance(transform.position, t.position) > 1f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, t.position, Time.deltaTime * speed);
+                yield return new WaitForEndOfFrame();
+            }
+        
+        
+        }
+
+        #endregion
+
+        #region Animation
+
+        public void StartInitiAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+               
+        }
+
+        #endregion
 
         #region DEBUG
         [NaughtyAttributes.Button]
         private void SwitchInit()
-        { 
+        {
             SwitchState(BossAction.INIT);
+        }
+        [NaughtyAttributes.Button]
+        private void SwitchWalk()
+        {
+            SwitchState(BossAction.WALK);
         }
 
         #endregion
