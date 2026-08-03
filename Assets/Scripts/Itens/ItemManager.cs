34b5c8c70e48da138e271cec63d24e480bdb3e49
Wilber <wilber.sa@hotmail.com>
2026-08-02 22:13:23 -0300

    Create Item Manager script

---
+++ Assets/Scripts/Itens/ItemManager.cs
@@ -0,0 +1,27 @@
+using EDGEE.Core.Singleton;
+using UnityEngine;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    public SOFloat totalCoins;
+
+    void Start()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        if (totalCoins != null)
+            totalCoins.valueFloat = 0;
+    }
+
+    public void AddCoins(float amount = 0.5f)
+    {
+        if (totalCoins != null)
+        {
+            totalCoins.valueFloat += amount;
+            Debug.Log("Colected Coin! Total: " + totalCoins.valueFloat);
+        }
+    }
+}
